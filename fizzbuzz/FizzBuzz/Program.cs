265fc47290c6cd04557c5ee21d31f17630e46d85
Alex Gosselin <2355055@csfoy.ca>
2023-09-28 16:45:37 -0400

    modif du code

--- fizzbuzz/FizzBuzz/Program.cs
+++ fizzbuzz/FizzBuzz/Program.cs
@@ -6,9 +6,13 @@ namespace FizzBuzzDemo
         // This is where the program will start
         static void Main(string[] args)
         {
+            Console.Write("Entrez un nombre");
+            int max = int.Parse(Console.ReadLine());
             // This is the beginning of the program 
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= max; i++)
             {
+                
+
                 if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
